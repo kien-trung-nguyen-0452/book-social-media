8191a089769ff4184d59fbba1535da57145566ff
kien0452 <kienvipdeptrai245@gmail.com>
2025-05-09 15:23:21 +0700

    add crawl service

---
+++ crawl-service/src/test/java/org/example/crawlservice/CrawlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crawlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
